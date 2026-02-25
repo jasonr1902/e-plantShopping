faae64838454c3e0052a76e5cb0ba437391ce024
jasonr1902 <jason.royer1902@gmail.com>
2026-02-25 16:41:48 -0500

    Added everything else

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,38 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    var total = 0;
+    cart.forEach((item) => {
+        var quantity = item.quantity;
+        var cost = parseFloat(item.cost.substring(1));
+        total += cost * quantity;
+    })
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}))
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1)
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}))
+    else dispatch(removeItem({item}));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({item}));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.substring(1)) * item.quantity;
   };
 
   return (
