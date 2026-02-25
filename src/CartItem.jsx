18dcaa84d43364891a8fc80384e7ba94d3c15c37
jasonr1902 <jason.royer1902@gmail.com>
2026-02-25 16:49:35 -0500

    Fixed removeItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -31,11 +31,11 @@ const CartItem = ({ onContinueShopping }) => {
   const handleDecrement = (item) => {
     if (item.quantity > 1)
     dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}))
-    else dispatch(removeItem({item}));
+    else dispatch(removeItem(item.name));
   };
 
   const handleRemove = (item) => {
-    dispatch(removeItem({item}));
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
